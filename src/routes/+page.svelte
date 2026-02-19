7a2cdc9dda39f6ba688ac4901651a32497ad7047
David Lange <lange@finiam.com>
2026-02-19 11:28:34 +0000

    responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,27 +14,30 @@
 			<rect x="{pos.x}%" y="{pos.y}%" class="rect"></rect>
 		{/each}
 	</svg>
+
+	<div class="main">
+		<div class="branding">
+			<img src={paren_left} alt="(" class="paren" />
+			<img src={logo} alt="Coimbra.js" class="logo" />
+			<img src={paren_right} alt=")" class="paren" />
+		</div>
+
+		<iframe
+			title="Luma event"
+			src="https://luma.com/embed/event/evt-5ECNA9iNMrr6ynW/simple"
+			width="784"
+			height="400"
+			frameborder="0"
+			allow="fullscreen; payment"
+			aria-hidden="false"
+		></iframe>
+	</div>
+
 	<img
 		src={sponsors}
 		alt="Powered by Subvisual, Remote Crew, and Pink Room"
 		class="sponsors"
 	/>
-
-	<div class="branding">
-		<img src={paren_left} alt="(" class="paren" />
-		<img src={logo} alt="Coimbra.js" class="logo" />
-		<img src={paren_right} alt=")" class="paren" />
-	</div>
-
-	<iframe
-		title="Luma event"
-		src="https://luma.com/embed/event/evt-5ECNA9iNMrr6ynW/simple"
-		width="784"
-		height="408"
-		frameborder="0"
-		allow="fullscreen; payment"
-		aria-hidden="false"
-	></iframe>
 </div>
 
 <style>
@@ -44,8 +47,6 @@
 
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
 
 		background-color: var(--brand-maroon);
 
@@ -54,37 +55,70 @@
 		}
 	}
 
-	.branding {
+	.main {
+		flex: 1 0 auto;
+
 		display: flex;
+		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 	}
 
+	.branding {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: fit-content;
+	}
+
 	.sponsors {
-		position: absolute;
-		top: 5vh;
-		right: 0;
-		left: 0;
-		margin: auto;
+		margin: 0 auto;
 		height: 24px;
+		max-width: 90%;
+		margin-bottom: 10px;
+
+		@media (min-width: 1440px) {
+			margin-bottom: 20px;
+		}
 	}
 
 	.logo {
-		max-width: 580px;
-		margin: 0 20px;
+		max-width: 90%;
+		width: 580px;
+		margin: 20px 0 20px 0;
+
+		@media (min-width: 1000px) {
+			margin: 0 20px;
+		}
+
+		@media (min-width: 1440px) {
+			margin: 40px 20px;
+		}
 	}
 
 	.paren {
-		max-width: 150px;
+		width: 0px;
+		position: absolute;
+
+		@media (min-width: 1000px) {
+			width: 130px;
+		}
+
+		@media (min-width: 1440px) {
+			width: 150px;
+		}
 
 		&:first-of-type {
+			left: 0;
 			rotate: -3deg;
-			translate: 0 10%;
+			translate: -120px 10%;
 		}
 
 		&:last-of-type {
+			right: 0;
 			rotate: 3deg;
-			translate: 0 -10%;
+			translate: 120px -10%;
 		}
 	}
 
@@ -100,11 +134,24 @@
 			height: 40px;
 
 			translate: -4vmin;
+
+			@media (min-width: 1440px) {
+				width: 80px;
+				height: 80px;
+			}
 		}
 	}
 
 	iframe {
 		position: relative;
-		margin-bottom: 20vh;
+		max-width: 90%;
+
+		@media (min-width: 1440px) {
+			margin-bottom: 20vh;
+		}
+
+		@media (max-width: 1000px) {
+			height: stretch;
+		}
 	}
 </style>
